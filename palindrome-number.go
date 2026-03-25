ba8e3ea5afba41e37eb7b3380a180441ba0e12de
abdulrahmanhossam <abdulrahmn398@outlook.com>
2026-03-25 17:38:08 +0200

    palindrome-number

--- palindrome-number.go
+++ palindrome-number.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+//import "fmt"
 
-func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
 
-	result := twoSum(nums, target)
-	fmt.Println("Result:", result)
+	reversedHalf := 0
+
+	for x > reversedHalf {
+		reversedHalf = (reversedHalf * 10) + (x % 10)
+		x /= 10
+	}
+
+	return x == reversedHalf || x == reversedHalf/10
 }
+
+// func main() {
+// 	fmt.Println("Is 121 a palindrome?", isPalindrome(121))
+// 	fmt.Println("Is -121 a palindrome?", isPalindrome(-121))
+// 	fmt.Println("Is 10 a palindrome?", isPalindrome(10))
+// 	fmt.Println("Is 1221 a palindrome?", isPalindrome(1221))
+// }
