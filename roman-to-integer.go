a1fe8b486e336bc8647eae9b3f28a4a927d02e55
abdulrahmanhossam <abdulrahmn398@outlook.com>
2026-03-27 18:44:42 +0200

    roman-to-integer

---
+++ roman-to-integer.go
@@ -0,0 +1,38 @@
+package main
+
+// import "fmt"
+
+func romanToInt(s string) int {
+	romanMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	total := 0
+	lastValue := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		current := romanMap[s[i]]
+
+		if current < lastValue {
+			total = total - current
+		} else {
+			total = total + current
+		}
+
+		lastValue = current
+	}
+
+	return total
+}
+
+// func main() {
+// 	fmt.Println("III ->", romanToInt("III"))
+// 	fmt.Println("LVIII ->", romanToInt("LVIII"))
+// 	fmt.Println("MCMXCIV ->", romanToInt("MCMXCIV"))
+// }
